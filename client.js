9022b24c913945a7b7c084e2b41071ec703d0d2a
cphung1 <cphung97@gmail.com>
2020-02-12 21:03:04 +0000

    displays message when connected, also prints initals on server upon connection

--- client.js
+++ client.js
@@ -11,6 +11,12 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write("Name: cp")
+  })
+
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   })
