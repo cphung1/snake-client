0509a6cb22227f81ea17b1936c3d892498b452b5
cphung1 <cphung97@gmail.com>
2020-02-12 20:50:09 +0000

    added event handler when idle, server sends msg

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const net = require('net');
 
-const conn = net.createConnection({ 
-  host: '', // change to IP address
-  port: 3000
-});
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.177',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  })
+  
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
