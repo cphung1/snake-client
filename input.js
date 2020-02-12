d6cd88c16e3b39112e558e3335507651cfedfb19
cphung1 <cphung97@gmail.com>
2020-02-12 22:00:09 +0000

    included hotkey feature that displays message

--- input.js
+++ input.js
@@ -3,18 +3,21 @@ let connection;
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
-  }
+  };
   if (data === 'w') {
     connection.write("Move: up")
-  }
+  };
   if (data === 'a') {
     connection.write("Move: left")
-  }
+  };
   if (data === 's') {
     connection.write("Move: down")
   }
   if (data === 'd') {
     connection.write("Move: right")
+  };
+  if (data === 'z') {
+    connection.write("Say: move pls")
   }
 }
 
