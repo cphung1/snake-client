022e9f68ccff714f929aa86845ac54b8b790db94
cphung1 <cphung97@gmail.com>
2020-02-12 21:20:52 +0000

    commented out hard-coded move: up msg

--- client.js
+++ client.js
@@ -14,6 +14,9 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server')
     conn.write("Name: cp")
+    // setInterval(() => {
+    //   conn.write("Move: up")
+    // }, 50);
   })
 
 
